495382f133ca5ea77cedcd65aabbf782cad648a4
Dokyung An <tea-co@naver.com>
2024-12-26 10:32:23 +0900

    수정

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
 
 // MongoDB 연결
 mongoose.connect(process.env.MONGO_URI, {
